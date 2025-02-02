06ae909b23e2bc678de48236658933c802f7bb00
hamolicious <hamolicious@hamolicious.net>
2025-02-02 20:12:26 +0000

    close thread on exit

--- src/pipeline_manager/cli.py
+++ src/pipeline_manager/cli.py
@@ -17,17 +17,20 @@ Pipelines: TypeAlias = RESTObjectList | List[RESTObject]
 class PipelineManager(App):
     pipelines: reactive[list[Pipeline]] = reactive([])
     worker_last_update = 0.0
+    worker_running = True
     REQUEST_INTERVAL_SEC = 3
+
+    def on_unmount(self) -> None:
+        self.worker_running = False
+        self.log.info("Dismounting, Closing thread...")
 
     @work(exclusive=True, thread=True, name="Pipeline Updatetor")
     def start_pipeline_updator(self) -> None:
         self.log.info(f"Pipeline updator thread started")
         project = get_current_project()
 
-        while True:
+        while self.worker_running:
             elapsed = time.time() - self.worker_last_update
-            self.log.info(f"Elapsed: {elapsed}")
-
             if elapsed < self.REQUEST_INTERVAL_SEC:
                 time.sleep(0.5)
                 continue
